fe11b680358393a7dcb81076bde8287606b6abee
William Jesus <williamjesusti@gmail.com>
2022-03-23 20:36:36 -0300

    chore: add integration with backend application

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -2,16 +2,13 @@ import { Link } from "react-router-dom";
 
 import MovieScore from "components/MovieScore";
 
-function MovieCard() {
-  const movie = {
-    id: 1,
-    image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5,
-  };
+import { Movie } from "types/movie";
 
+type Props = {
+  movie: Movie;
+};
+
+function MovieCard({ movie }: Props) {
   return (
     <div>
       <img
@@ -21,7 +18,8 @@ function MovieCard() {
       />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
-        <MovieScore />
+        <MovieScore count={movie.count} score={movie.score} />
+
         <Link to={`/form/${movie.id}`}>
           <div className="btn btn-primary dsmovie-btn">Avaliar</div>
         </Link>
